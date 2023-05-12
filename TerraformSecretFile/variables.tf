fc228d30931f0f1f1876b5bf54da07176b8d2f92
Burcu Kayahan <120653619+burcuakdenizkayahan@users.noreply.github.com>
2023-05-12 04:58:30 +0300

    Add files via upload

---
+++ TerraformSecretFile/variables.tf
@@ -0,0 +1,11 @@
+variable "db_user" {
+  description = "adminusername"
+  type = string
+  sensitive = true
+}
+
+variable "db_pass" {
+  description = "adminpass"
+  type = string
+  sensitive = true
+}
